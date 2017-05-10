996dcccf92cf6d2d8757509b82a6d5c5fe6481b7
Justin Daining <justin@justindaining.com>
2017-05-10 09:14:31 -0600

    html bones in a basic function component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { IconComposeSolid } from 'instructure-icons';
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      <header className="card__header">
+        <img src="" />
+        <IconComposeSolid />
+        <div class="card__header-overlay--blue"></div>
+      </header>
+      <div className="card__content">
+        <h3>Course Title</h3>
+        <h4>SUB-TITLE</h4>
+        <div className="card__content-desc">
+          <p>Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. </p>
+        </div>
+      </div>
+      <footer className="card__footer">
+        <div className="card__footer-actions">
+
+        </div>
+        <div className="card__footer-stripe--blue"></div>
+      </footer>
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <Card />
       </div>
     );
   }
