510a70211efac79328f978707a7f95bae6c2a716
Justin Daining <justin@justindaining.com>
2017-05-10 10:27:29 -0600

    html/css mostly done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import { IconComposeSolid } from 'instructure-icons';
+import { IconMoreSolid } from 'instructure-icons';
+import { IconDocumentSolid } from 'instructure-icons';
+import { IconAnnouncementSolid } from 'instructure-icons';
+import { IconChatSolid } from 'instructure-icons';
+import { IconFolderSolid } from 'instructure-icons';
 import header1 from './header1.jpg';
+import header2 from './header2.jpg';
+import header3 from './header3.jpg';
+import header4 from './header4.jpg';
+import header5 from './header5.jpg';
 
 const Card = (props) => {
   return (
     <div className="card">
       <header className="card__header">
-        <img className="img--responsive" src={header1} />
-        <IconComposeSolid width="2em" height="2em" />
+        <img alt="Art History 3" className="img--responsive" src={props.header} />
+        <a href=""><IconComposeSolid /></a>
         <div className="card__header-overlay card__header-overlay--blue"></div>
       </header>
       <div className="card__content">
@@ -17,13 +26,15 @@ const Card = (props) => {
         <div className="card__content-desc">
           <p>Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. </p>
         </div>
+        <IconMoreSolid />
       </div>
-      <footer className="card__footer">
-        <div className="card__footer-actions">
-
-        </div>
-        <div className="card__footer-stripe card__footer-stripe--blue"></div>
-      </footer>
+      <div className="card__actions">
+        <a href=""><IconDocumentSolid /></a>
+        <a href=""><IconAnnouncementSolid /></a>
+        <a href=""><IconChatSolid /></a>
+        <a href=""><IconFolderSolid /></a>
+      </div>
+      <footer className="card__stripe card__stripe--blue"></footer>
     </div>
   );
 }
@@ -32,11 +43,11 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card color="blue" header={header1} />
+        <Card color="green" header={header2} />
+        <Card color="purple" header={header3} />
+        <Card color="red" header={header4} />
+        <Card color="orange" header={header5} />
       </div>
     );
   }
