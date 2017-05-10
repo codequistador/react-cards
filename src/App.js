e105829942a4b26c3572696d89e351d0fca7e7cf
Justin Daining <justin@justindaining.com>
2017-05-10 10:47:27 -0600

    responsive and more goodies... working on desc reveal

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { IconDocumentSolid } from 'instructure-icons';
 import { IconAnnouncementSolid } from 'instructure-icons';
 import { IconChatSolid } from 'instructure-icons';
 import { IconFolderSolid } from 'instructure-icons';
+import { IconXSolid } from 'instructure-icons';
 import header1 from './header1.jpg';
 import header2 from './header2.jpg';
 import header3 from './header3.jpg';
@@ -20,14 +21,9 @@ const Card = (props) => {
         <a href=""><IconComposeSolid /></a>
         <div className="card__header-overlay card__header-overlay--blue"></div>
       </header>
-      <div className="card__content">
-        <h3>Art History 3</h3>
-        <h4>ART HIS 3</h4>
-        <div className="card__content-desc">
-          <p>Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. </p>
-        </div>
-        <IconMoreSolid />
-      </div>
+
+      <CardContent />
+
       <div className="card__actions">
         <a href=""><IconDocumentSolid /></a>
         <a href=""><IconAnnouncementSolid /></a>
@@ -37,6 +33,29 @@ const Card = (props) => {
       <footer className="card__stripe card__stripe--blue"></footer>
     </div>
   );
+}
+
+class CardContent extends Component {
+  render() {
+    return (
+      <div>
+        <div className="card__content">
+          <h3>Art History 3</h3>
+          <h4>ART HIS 3</h4>
+          <div className="card__content-desc">
+            <p>Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. </p>
+          </div>
+          <IconMoreSolid />
+        </div>
+        <div className="card__reveal">
+          <IconXSolid />
+          <h3>Art History 3</h3>
+          <h4>ART HIS 3</h4>
+          <p>Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 class App extends Component {
