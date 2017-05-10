9312b7daf58992acf9ac6de89e056c108b584df9
Justin Daining <justin@justindaining.com>
2017-05-10 11:19:35 -0600

    dynamic variables passed into the card component

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const Card = (props) => {
         <div className={`card__header-overlay card__header-overlay--${props.color}`}></div>
       </header>
 
-      <CardContent />
+      <CardContent {...props} />
 
       <div className="card__actions">
         <a href=""><IconDocumentSolid /></a>
@@ -47,18 +47,18 @@ class CardContent extends Component {
     return (
       <div>
         <div className="card__content">
-          <h3>Art History 3</h3>
-          <h4>ART HIS 3</h4>
+          <h3>{this.props.title}</h3>
+          <h4>{this.props.subTitle}</h4>
           <div className="card__content-desc">
-            <p>Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. </p>
+            <p>{this.props.desc}</p>
           </div>
           <IconMoreSolid onClick={this.revealFullDesc.bind(this)} />
         </div>
         <div className={`card__reveal ${this.state.isRevealed}`}>
           <IconXSolid  onClick={this.revealFullDesc.bind(this)} />
-          <h3>Art History 3</h3>
-          <h4>ART HIS 3</h4>
-          <p>Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. </p>
+          <h3>{this.props.title}</h3>
+          <h4>{this.props.subTitle}</h4>
+          <p>{this.props.desc}</p>
         </div>
       </div>
     );
@@ -69,11 +69,11 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <Card color="blue" header={header1} />
-        <Card color="green" header={header2} />
-        <Card color="purple" header={header3} />
-        <Card color="red" header={header4} />
-        <Card color="orange" header={header5} />
+        <Card color="blue" header={header1} title="Art History 3" subTitle="ART HIS 3" desc="Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony." />
+        <Card color="green" header={header2} title="Gym" subTitle="PE 101" desc="Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony." />
+        <Card color="purple" header={header3} title="Psychology 400" subTitle="PS 400" desc="Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony." />
+        <Card color="red" header={header4} title="Programming 301" subTitle="PROG 301" desc="Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony." />
+        <Card color="orange" header={header5} title="Study Hall" subTitle="STUDY" desc="Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony." />
       </div>
     );
   }
