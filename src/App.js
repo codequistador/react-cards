1de8ecb723885609a22a57e529300e3ad6522da5
Justin Daining <justin@justindaining.com>
2017-05-10 09:53:54 -0600

    one hour ish

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import { IconComposeSolid } from 'instructure-icons';
+import header1 from './header1.jpg';
 
 const Card = (props) => {
   return (
     <div className="card">
       <header className="card__header">
-        <img src="" />
-        <IconComposeSolid />
-        <div class="card__header-overlay--blue"></div>
+        <img className="img--responsive" src={header1} />
+        <IconComposeSolid width="2em" height="2em" />
+        <div className="card__header-overlay card__header-overlay--blue"></div>
       </header>
       <div className="card__content">
-        <h3>Course Title</h3>
-        <h4>SUB-TITLE</h4>
+        <h3>Art History 3</h3>
+        <h4>ART HIS 3</h4>
         <div className="card__content-desc">
           <p>Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. </p>
         </div>
@@ -21,7 +22,7 @@ const Card = (props) => {
         <div className="card__footer-actions">
 
         </div>
-        <div className="card__footer-stripe--blue"></div>
+        <div className="card__footer-stripe card__footer-stripe--blue"></div>
       </footer>
     </div>
   );
@@ -31,6 +32,10 @@ class App extends Component {
   render() {
     return (
       <div className="container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
       </div>
     );
